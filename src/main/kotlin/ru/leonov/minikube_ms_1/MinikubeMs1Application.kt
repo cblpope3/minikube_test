eb0fb7c475af303f9a1ac75f18683ba6a8fde354
Nickolay Leonov <nickolayshmakolay@gmail.com>
2022-12-20 09:21:07 +0300

    Initial commit.

---
+++ src/main/kotlin/ru/leonov/minikube_ms_1/MinikubeMs1Application.kt
@@ -0,0 +1,11 @@
+package ru.leonov.minikube_ms_1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MinikubeMs1Application
+
+fun main(args: Array<String>) {
+	runApplication<MinikubeMs1Application>(*args)
+}
